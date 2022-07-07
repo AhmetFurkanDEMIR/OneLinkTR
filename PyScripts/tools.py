3c52be06ce73103ecc3d6efa81ffef76836dd594
Ahmet Furkan DEMIR <54184905+AhmetFurkanDEMIR@users.noreply.github.com>
2022-07-07 09:32:29 +0300

    Update tools.py

--- PyScripts/tools.py
+++ PyScripts/tools.py
@@ -1,11 +1,18 @@
 import psycopg2
 from functools import wraps
-from flask import session, redirect, url_for
+from flask import session, redirect, url_for, Flask
+from flask_recaptcha import ReCaptcha
 
-global ServerIp
+# Flask app
+app = Flask(__name__,template_folder='../templates', static_folder='../static')
+app.config['UPLOAD_PATH'] = 'static/uploads'
+app.secret_key = b'_softforrange***d*'
+app.config['RECAPTCHA_SITE_KEY'] = 'RECAPTCHA_SITE_KEY' # <-- Add your site key
+app.config['RECAPTCHA_SECRET_KEY'] = 'RECAPTCHA_SECRET_KEY' # <-- Add your secret key
+
+recaptcha = ReCaptcha(app)
+
 global ServerPort
-
-
 ServerPort = 5000
 global myUrl
 myIp = "192.168.1.33"
@@ -33,8 +40,8 @@ conn = psycopg2.connect(
     port="5432",
     user="postgres",
     password="123456789Zz.")
-
 """
+
 cursor = conn.cursor()
 
 def login_required(f):
